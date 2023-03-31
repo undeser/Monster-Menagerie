48fbcac6be4a21ef438e2c489f1248d5b624bbb5
Luther Tan <76609402+luthertan98@users.noreply.github.com>
2023-03-31 14:36:12 +0800

    Done with LPstaking.sol

--- contracts/LPstaking.sol
+++ contracts/LPstaking.sol
@@ -1,72 +1,66 @@
 pragma solidity ^0.8.0;
 
-import './IERC20.sol';
+import './ERC20.sol';
 import './SafeMath.sol';
 
 contract LPstaking {
-    using SafeMath for uint256;
-    address public lpToken;
-    mapping(address => uint256) public balances;
-    mapping (address => uint256) public userRewardPerTokenPaid;
-    mapping (address => uint256) public rewards;
+    ERC20 public lpToken;
+    Gem public gem;
+    uint256 public rewardPool = 100000 * 10**18; // 100,000 tokens with 18 decimals
+    uint256 public totalRewardDistributed;
+
+    struct StakeInfo {
+        uint256 amount;
+        uint256 rewardDebt;
+    }
+
+    mapping(address => StakeInfo) public stakes;
     uint256 public totalStaked;
-    uint256 public rewardRate;
-    uint256 public lastUpdateTime;
-    uint256 public rewardPerTokenStored;
-    uint256 public periodFinish;
 
-    constructor (address lpTokenAddress, uint256 stakingRewardRate) {
-        lpToken = lpTokenAddress;
-        rewardRate = stakingRewardRate;
-        lastUpdateTime = block.timestamp;
-        periodFinish = block.timestamp.add(30 days); 
+    event Deposit(address indexed user, uint256 amount);
+    event Withdraw(address indexed user, uint256 amount);
+    event ClaimReward(address indexed user, uint256 reward);
+
+    constructor(address _lpToken, address _rewardToken) {
+        lpToken = ERC20(_lpToken);
+        rewardToken = Gem(_rewardToken);
     }
 
-    function stake(uint256 amount) public {
-        require(amount > 0, 'Cannot stake 0 LP tokens');
+    function deposit(uint256 amount) public {
         updateReward(msg.sender);
-        IERC20(lpToken).transferFrom(msg.sender, address(this), amount);
-        balances[msg.sender] = balances[msg.sender].add(amount);
-        totalStaked = totalStaked.add(amount);
+        stakes[msg.sender].amount += amount;
+        totalStaked += amount;
+        lpToken.TransferFrom(msg.sender, address(this), amount);
+        emit Deposit(msg.sender, amount);
     }
 
-    function unstake(uint256 amount) public {
-        require(amount > 0, 'Cannot unstake 0 LP tokens');
+    function withdraw(uint256 amount) public {
+        require(stakes[msg.sender].amount >= amount, "Withdraw: insufficient balance");
         updateReward(msg.sender);
-        IERC20(lpToken).transfer(msg.sender, amount);
-        balances[msg.sender] = balances[msg.sender].sub(amount);
-        totalStaked = totalStaked.sub(amount);
+        stakes[msg.sender].amount -= amount;
+        totalStaked -= amount;
+        lpToken.Transfer(msg.sender, amount);
+        emit Withdraw(msg.sender, amount);
     }
 
-    function updateReward(address account) internal {
-        rewardPerTokenStored = rewardPerToken(); // The staking yield
-        lastUpdateTime = lastTimeRewardApplicable(); 
-        if (account != address(0)) {
-            balances[account] = earned(account);
-        }
+    function claimReward() public {
+        updateReward(msg.sender);
+        uint256 reward = stakes[msg.sender].rewardDebt;
+        stakes[msg.sender].rewardDebt = 0;
+        require(totalRewardDistributed + reward <= rewardPool, "Not enough rewards left in the pool");
+        totalRewardDistributed += reward;
+        rewardToken.Transfer(msg.sender, reward);
+        emit ClaimReward(msg.sender, reward);
     }
 
-    function rewardPerToken() public view returns (uint256) {
-        if (totalStaked == 0) {
-            return rewardPerTokenStored;
-        }
-        return rewardPerTokenStored.add(
-            lastTimeRewardApplicable()
-                .sub(lastUpdateTime)
-                .mul(rewardRate)
-                .mul(1e18)
-                .div(totalStaked)
-        );
+    function updateReward(address user) internal {
+        uint256 reward = (stakes[user].amount * currentAPR()) / 1e18;
+        stakes[user].rewardDebt += reward;
+    }
+    // reward distribution for a year
+    function currentAPR() public view returns (uint256) {
+        uint256 remainingRewardPool = rewardPool - totalRewardDistributed;
+        return (remainingRewardPool * 1e18) / totalStaked;
     }
 
-    function earned(address account) public view returns (uint256) {
-        return balances[account]
-            .mul(rewardPerToken().sub(userRewardPerTokenPaid[account]))
-            .div(1e18)
-            .add(rewards[account]);
-    }
-
-    function lastTimeRewardApplicable() public view returns (uint256) {
-        return block.timestamp < periodFinish ? block.timestamp : periodFinish;
-    }
 }
