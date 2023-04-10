24e8fa2ca68612be04fd648700e4c7a583e2a7e4
bryanwonggy <bryanwong040500@gmail.com>
2023-04-10 12:15:19 +0800

    properly documented comments for gem.sol

--- contracts/Gem.sol
+++ contracts/Gem.sol
@@ -3,59 +3,89 @@ pragma solidity ^0.8.17;
 
 import "./ERC20.sol"; 
 
+/**
+ * @title Gem
+ * @dev Gem is the ERC20 token used for MonsterMenagerie.
+ */
 contract Gem is ERC20("BEAST GEM", "BGM") {
-    // ERC20 erc20Contract;
     uint256 supplyLimit;
     uint256 currentSupply;
     address owner;
     
+    /**
+     * @dev Sets the values for owner of the contract and supply limit
+     */
     constructor() {
         owner = msg.sender;
         supplyLimit = 1000000 * 1000000000000000000;
     }
 
-    // Function to mint Gems with ETH
+    /**
+     * @dev Mints Gems with ETH
+     */
     function getGems() public payable {
         uint256 amt = msg.value * 1000;
         require(totalSupply() + amt < supplyLimit, "Warning: Insufficient Gems!");
-        // erc.mint(address account, uint256 amount);
         mint(msg.sender, amt);
     }
 
-    // Function to check balance of user's gems
+    /**
+     * @dev Getter for amount of gems held by the caller of the function
+     */
     function checkGems() public view returns (uint256) {
         return balanceOf(msg.sender) / 1000000000000000000;
     }
 
-    // Function to check balance of an address' gems
-    function checkGemsOf(address myAdd) public view returns (uint256) {
-        return balanceOf(myAdd) / 1000000000000000000;
+    /**
+     * @dev Getter for amount of gems held by an address
+     * @param user Address of user of interest
+     */
+    function checkGemsOf(address user) public view returns (uint256) {
+        return balanceOf(user) / 1000000000000000000;
     }
 
-    // Function to transfer gems to a recipient
+    /**
+     * @dev Transfer gems from function caller to recipient
+     * @param recipient Address of recipient
+     * @param value Amount of gems to transfer
+     */
     function transferGems(address recipient, uint256 value) public returns (bool) {
         return transfer(recipient, value * 1000000000000000000);
     }
 
-    // Function to transfer gems from an address to another address
+    /**
+     * @dev Transfer gems from an address to another address
+     * @param from Address of sender
+     * @param to Address of recipient
+     * @param amt Amount of gems to transfer
+     */
     function transferGemsFrom(address from, address to, uint256 amt) public {
         require(allowance(from, msg.sender) > amt * 1000000000000000000, "Warning: You are not allowed to transfer!");
         transferFrom(from, to, amt * 1000000000000000000);
     }
 
-    // Function to give an address approval to transfer gems
-    function giveGemApproval(address receipt, uint256 amt) public {
-        approve(receipt, amt * 1000000000000000000);
+    /**
+     * @dev Give an address approval to transfer a specified amount of gems
+     * @param recipient Address to be given approval
+     * @param amt Amount of gems to be approved to the address
+     */
+    function giveGemApproval(address recipient, uint256 amt) public {
+        approve(recipient, amt * 1000000000000000000);
     }
 
-    // Function to check the allowance given to the spender by the user
+    /**
+     * @dev Check allowance given to spender by the user
+     * @param user Address of the owner of the gems
+     * @param spender Address of the spender of the gems
+     */
     function checkGemAllowance(address user, address spender) public view returns (uint256) {
         return allowance(user, spender) / 1000000000000000000;
     }
 
-    // Function to track current total gem supply
+    /**
+     * @dev Track current total gem supply
+     */
     function currentGemSupply() public view returns (uint256) {
         return totalSupply() / 1000000000000000000;
     }
-
 }
